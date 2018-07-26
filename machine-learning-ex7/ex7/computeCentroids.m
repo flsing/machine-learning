a2ee310cc597cc51ce4995753402eb014d7f9464
Felix Singerman <flsingerman@gmail.com>
2018-07-26 19:15:58 -0400

    finished ex7

--- machine-learning-ex7/ex7/computeCentroids.m
+++ machine-learning-ex7/ex7/computeCentroids.m
@@ -1,7 +1,7 @@
 function centroids = computeCentroids(X, idx, K)
-%COMPUTECENTROIDS returns the new centroids by computing the means of the 
+%COMPUTECENTROIDS returns the new centroids by computing the means of the
 %data points assigned to each centroid.
-%   centroids = COMPUTECENTROIDS(X, idx, K) returns the new centroids by 
+%   centroids = COMPUTECENTROIDS(X, idx, K) returns the new centroids by
 %   computing the means of the data points assigned to each centroid. It is
 %   given a dataset X where each row is a single data point, a vector
 %   idx of centroid assignments (i.e. each entry in range [1..K]) for each
@@ -26,15 +26,30 @@ centroids = zeros(K, n);
 % Note: You can use a for-loop over the centroids to compute this.
 %
 
+centroids = zeros(K, size(X,2));
 
+for i = 1:K
+    centroids(i, :) = mean(X(find(idx == i), :));
+end
 
+% my solution was given full marks in submission but was not working for a Part
+% so i found this code online https://github.com/everpeace/ml-class-assignments/blob/master/ex7.K-Means_Clustering_and_PCA/mlclass-ex7/computeCentroids.m
+% for testing purposes
 
-
-
+%for k = 1:K
+%  num_k = 0;
+%  sum = zeros(n, 1);
+%  for i = 1:m
+%    if ( idx(i) == k )
+%      sum = sum + X(i, :)';
+%      num_k = num_k + 1;
+%    end
+% end
+%  centroids(k, :) = (sum/num_k)';
+%end
 
 
 % =============================================================
 
 
 end
-
--- machine-learning-ex7/ex7/findClosestCentroids.m
+++ machine-learning-ex7/ex7/findClosestCentroids.m
@@ -1,7 +1,7 @@
 function idx = findClosestCentroids(X, centroids)
 %FINDCLOSESTCENTROIDS computes the centroid memberships for every example
 %   idx = FINDCLOSESTCENTROIDS (X, centroids) returns the closest centroids
-%   in idx for a dataset X where each row is a single example. idx = m x 1 
+%   in idx for a dataset X where each row is a single example. idx = m x 1
 %   vector of centroid assignments (i.e. each entry in range [1..K])
 %
 
@@ -15,14 +15,25 @@ idx = zeros(size(X,1), 1);
 % Instructions: Go over every example, find its closest centroid, and store
 %               the index inside idx at the appropriate location.
 %               Concretely, idx(i) should contain the index of the centroid
-%               closest to example i. Hence, it should be a value in the 
+%               closest to example i. Hence, it should be a value in the
 %               range 1..K
 %
 % Note: You can use a for-loop over the examples to compute this.
 %
 
+m = size(X,1);
 
-
+for i = 1:m
+     idx(i)=1;
+     diff = sqrt(sum((X(i,:) - centroids(1,:)).^2));
+     for j = 2:K
+        newDiff = sqrt(sum((X(i,:) - centroids(j,:)).^2));
+        if diff > newDiff
+            diff = newDiff;
+            idx(i) = j;
+        end
+    end
+end
 
 
 
@@ -30,4 +41,3 @@ idx = zeros(size(X,1), 1);
 % =============================================================
 
 end
-
